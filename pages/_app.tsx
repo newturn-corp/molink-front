4c7ea373de0c9143ea3e2a09f2aa3f42232e9aff
huseong <huseong.lee@deering,co>
2021-12-10 05:49:09 +0900

    build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,15 @@ import Head from 'next/head'
 import '../styles/global.css'
 import { AppProps } from 'next/dist/shared/lib/router/router'
 
-function SafeHydrate ({ children }) {
-    return (
-        <div suppressHydrationWarning>
-            {typeof window === 'undefined' ? null : children}
-        </div>
-    )
-}
-
 const App = ({ Component, pageProps }: AppProps) => {
     return (
-        <SafeHydrate>
+        <>
             <Head>
                 <title>{'good'}</title>
                 <link rel='shortcut icon' href='/favicon.svg' />
             </Head>
             <Component {...pageProps} />
-        </SafeHydrate>
+        </>
     )
 }
 
