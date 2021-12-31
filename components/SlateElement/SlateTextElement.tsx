a389db22d9c576c9bc825ee584642eb0210c6ff3
huseong <huseong.lee@deering,co>
2022-01-01 07:52:54 +0900

    Notion 같은 / 추가

--- components/SlateElement/SlateTextElement.tsx
+++ components/SlateElement/SlateTextElement.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Range } from 'slate'
+import { useSelected, useSlate } from 'slate-react'
 
 export const SlateTextElement: React.FC<{
     attributes,
     children,
     element
   }> = ({ attributes, children, element }) => {
+      const selected = useSelected()
+      const editor = useSlate()
+
+      const selection = editor.selection
+      const isSelectionCollapsed = selection !== null && Range.isCollapsed(editor.selection)
+      const isEmpty = children[0].props.text.text === '' && children.length === 1
+      console.log(isEmpty)
+      console.log(selected && isEmpty && isSelectionCollapsed)
       return (
-          <p className={`text ${element.category}`} {...attributes}>
+          <p className={ selected && isEmpty && isSelectionCollapsed
+              ? `selected-empty-text text ${element.category}`
+              : `text ${element.category}`} {...attributes}>
               {children}
           </p>
       )
