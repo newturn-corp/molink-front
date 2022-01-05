d90e965a9d8663f2601ae3dddf827363879e6793
huseong <huseong.lee@deering,co>
2022-01-05 21:22:13 +0900

    타임존 수정

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ module.exports = (phase, { defaultConfig }) => {
     defaultConfig.env = {
         IS_DEV: phase === PHASE_DEVELOPMENT_SERVER,
         SERVER_BASE_URL: process.env.SERVER_BASE_URL,
-        FRONT_HOST_NAME: process.env.FRONT_HOST_NAME
+        FRONT_HOST_NAME: process.env.FRONT_HOST_NAME,
+        TZ: 'Asia/Seoul'
     }
     defaultConfig.future = { webpack5: true }
     defaultConfig.swcMinify = true
