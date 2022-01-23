1e85017089cdfd90fa52f504fd3359e4290f5b1d
molink-huseong <huseong.lee@molink.life>
2022-01-23 23:56:45 +0900

    빌드 되도록

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-import { SidebarLayout } from '../components/SideBarLayout'
 // import { Content } from '../components/Content'
 
 const Index = () => {
     return <>
         Newturn Corporation
-        {/*<SidebarLayout />*/}
+        {/* <SidebarLayout /> */}
         {/* <Content/> */}
     </>
 }
