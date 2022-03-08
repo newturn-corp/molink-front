a9d7f7eb945e24a3a9a8a44658a7626bb3ff76dd
molink-huseong <huseong.lee@molink.life>
2022-03-08 20:06:16 +0900

    이메일 인증하는 부분 수정

--- pages/auth/email-auth.tsx
+++ pages/auth/email-auth.tsx
@@ -6,15 +6,9 @@ import RoutingManager, { Page } from '../../manager/global/RoutingManager'
 const EmailAuth = () => {
     const key = new URLSearchParams(window.location.search).get('key')
     useEffect(() => {
-        AuthManager.verifyEmail(key).then((res) => {
-            if (!res.success) {
-                FeedbackManager.showFeedback(NOTIFICATION_TYPE.ERROR, '이메일 인증 만료', '이미 만료된 인증입니다.', 10)
-            } else {
-                FeedbackManager.showFeedback(NOTIFICATION_TYPE.SUCCESS, '이메일 인증 성공!', '', 10)
-                RoutingManager.moveTo(Page.SignIn)
-            }
-        })
-    }, [])
+        AuthManager.verifyEmail(key)
+    },
+    [])
     return <></>
 }
 
