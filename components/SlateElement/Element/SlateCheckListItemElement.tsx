922d8374c09bb4f0b577a30497b6518748596699
molink-huseong <huseong.lee@molink.life>
2022-05-26 01:18:47 +0900

    Check List 스타이 ㄹ수정 및 버그 수정

--- components/SlateElement/Element/SlateCheckListItemElement.tsx
+++ components/SlateElement/Element/SlateCheckListItemElement.tsx
@@ -11,10 +11,28 @@ export const SlateCheckListItemElement: React.FC<{
       const editor = useSlateStatic()
       const readOnly = useReadOnly()
       const { checked } = element
+      const checkListIndex = []
+      for (let i = 0; i < element.children.length; i++) {
+          if (element.children[i].type === 'check-list') {
+              checkListIndex.push(i)
+          }
+      }
+      const checkListChildren = []
+      const otherChildren = []
+      for (let i = 0; i < children.length; i++) {
+          if (checkListIndex.includes(i)) {
+              checkListChildren.push(children[i])
+          } else {
+              otherChildren.push(children[i])
+          }
+      }
+      console.log(children)
+      console.log(element.children)
       return (
-          <div
-              {...attributes}
-              className={css`
+          <div>
+              <div
+                  {...attributes}
+                  className={css`
         display: flex;
         flex-direction: row;
         align-items: stretch;
@@ -23,32 +41,32 @@ export const SlateCheckListItemElement: React.FC<{
           margin-top: 0;
         }
       `}
-          >
-              <span
-                  contentEditable={false}
-                  className={css`
-          margin-right: 0.75em;
-        `}
               >
-                  <input
-                      type="checkbox"
-                      style={{
-                          marginTop: 9
-                      }}
-                      checked={checked}
-                      onChange={event => {
-                          const path = ReactEditor.findPath(editor, element)
-                          const newProperties = {
-                              checked: event.target.checked
-                          }
-                          Transforms.setNodes(editor, newProperties, { at: path })
-                      }}
-                  />
-              </span>
-              <span
-                  contentEditable={!readOnly}
-                  suppressContentEditableWarning={true}
-                  className={css`
+                  <span
+                      contentEditable={false}
+                      className={css`
+          margin-right: 0.5em;
+        `}
+                  >
+                      <input
+                          type="checkbox"
+                          style={{
+                              marginTop: 9
+                          }}
+                          checked={checked}
+                          onChange={event => {
+                              const path = ReactEditor.findPath(editor, element)
+                              const newProperties = {
+                                  checked: event.target.checked
+                              }
+                              Transforms.setNodes(editor, newProperties, { at: path })
+                          }}
+                      />
+                  </span>
+                  <span
+                      contentEditable={!readOnly}
+                      suppressContentEditableWarning={true}
+                      className={css`
           flex: 1;
           opacity: ${checked ? 0.666 : 1};
           text-decoration: ${!checked ? 'none' : 'line-through'};
@@ -57,9 +75,11 @@ export const SlateCheckListItemElement: React.FC<{
             outline: none;
           }
         `}
-              >
-                  {children}
-              </span>
+                  >
+                      {otherChildren}
+                  </span>
+              </div>
+              {checkListChildren}
           </div>
       )
   }
