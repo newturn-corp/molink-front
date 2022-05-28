bfc725652e368ee8e8c2780b3a641111ca9bed44
molink-huseong <huseong.lee@molink.life>
2022-05-28 12:14:39 +0900

    태그에 안내 툴팁 추가

--- components/Blog/EditorPage/PageTagList.tsx
+++ components/Blog/EditorPage/PageTagList.tsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 import { observer } from 'mobx-react'
 import { isBrowser } from 'react-device-detect'
 import EditorManager from '../../../manager/Blog/EditorManager'
-import { Tag } from 'antd'
+import { Tag, Tooltip } from 'antd'
 import NotificationManager from '../../../manager/global/NotificationManager'
 import FeedbackManager, { NOTIFICATION_TYPE } from '../../../manager/global/FeedbackManager'
 
 export const PageTagList: React.FC<{
 }> = observer(() => {
     const [newTag, setNewTag] = useState('')
-    console.log(EditorManager.tags)
+    const [isTagFocus, setIsTagFocus] = useState(false)
     return <div
         className={'page-tag-list-container'}
     >
@@ -31,28 +31,36 @@ export const PageTagList: React.FC<{
         {
             EditorManager.editable &&
             !EditorManager.isLocked
-                ? <input
-                    className={'tag-input'}
-                    placeholder={'태그 입력'}
-                    value={newTag}
-                    onChange={(event) => {
-                        setNewTag(event.target.value)
-                    }}
-                    onKeyDown={(event) => {
-                        if (newTag !== '') {
-                            if (event.key === 'Enter' || event.key === ',') {
-                                // 중복이 없으면
-                                event.preventDefault()
-                                if (EditorManager.tags.filter(v => v === newTag).length === 0) {
-                                    EditorManager.addTag(newTag)
-                                } else {
-                                    FeedbackManager.showFeedback(NOTIFICATION_TYPE.ERROR, '이미 태그가 존재합니다.', '', 5)
+                ? <Tooltip
+                    title={'엔터 또는 쉼표로 태그 입력'}
+                    placement={'bottomLeft'}
+                    visible={isTagFocus}
+                >
+                    <input
+                        className={'tag-input'}
+                        placeholder={'태그 입력'}
+                        value={newTag}
+                        onChange={(event) => {
+                            setNewTag(event.target.value)
+                        }}
+                        onFocus={() => setIsTagFocus(true)}
+                        onBlur={() => setIsTagFocus(false)}
+                        onKeyDown={(event) => {
+                            if (newTag !== '') {
+                                if (event.key === 'Enter' || event.key === ',') {
+                                    // 중복이 없으면
+                                    event.preventDefault()
+                                    if (EditorManager.tags.filter(v => v === newTag).length === 0) {
+                                        EditorManager.addTag(newTag)
+                                    } else {
+                                        FeedbackManager.showFeedback(NOTIFICATION_TYPE.ERROR, '이미 태그가 존재합니다.', '', 5)
+                                    }
+                                    setNewTag('')
                                 }
-                                setNewTag('')
                             }
-                        }
-                    }}
-                />
+                        }}
+                    />
+                </Tooltip>
                 : <></>
         }
     </div>
