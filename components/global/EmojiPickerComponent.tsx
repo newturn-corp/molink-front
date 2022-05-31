a6c2155b27fd8d7e547ba2fc5d8ca5a6ca2444a9
molink-huseong <huseong.lee@molink.life>
2022-05-31 11:55:39 +0900

    옆에 스크롤바 생기는 이슈 수정

--- components/global/EmojiPickerComponent.tsx
+++ components/global/EmojiPickerComponent.tsx
@@ -24,43 +24,27 @@ export const EmojiPickerComponent: React.FC<{
 
       return <Portal>
           <div
+              ref={pickerRef}
+              className={'emoji-picker'}
               style={{
                   userSelect: EmojiPicker.isOpen ? undefined : 'none',
-                  zIndex: 10000,
-                  width: '100%',
-                  height: '100%'
-              }}
-              onClick={() => {
-                  EmojiPicker.close()
-              }}
-              onScroll={(event) => {
-                  event.stopPropagation()
-                  event.preventDefault()
+                  opacity: EmojiPicker.isOpen ? '1' : '0'
               }}
           >
-              <div
-                  ref={pickerRef}
-                  className={'emoji-picker'}
-                  style={{
-                      userSelect: EmojiPicker.isOpen ? undefined : 'none',
-                      opacity: EmojiPicker.isOpen ? '1' : '0'
+              <EmojiPickerNoSSRWrapper
+                  groupNames={{
+                      smileys_people: '사람들',
+                      animals_nature: '자연과 동물',
+                      food_drink: '음식',
+                      travel_places: '여행과 장소',
+                      activities: '활동',
+                      objects: '물건',
+                      symbols: '심볼',
+                      flags: '국기',
+                      recently_used: '최근 사용한 이모지'
                   }}
-              >
-                  <EmojiPickerNoSSRWrapper
-                      groupNames={{
-                          smileys_people: '사람들',
-                          animals_nature: '자연과 동물',
-                          food_drink: '음식',
-                          travel_places: '여행과 장소',
-                          activities: '활동',
-                          objects: '물건',
-                          symbols: '심볼',
-                          flags: '국기',
-                          recently_used: '최근 사용한 이모지'
-                      }}
-                      disableSearchBar={EmojiPicker.disableSearchbar}
-                      onEmojiClick={(event, data) => EmojiPicker.onEmojiClick(event, data)} />
-              </div>
+                  disableSearchBar={EmojiPicker.disableSearchbar}
+                  onEmojiClick={(event, data) => EmojiPicker.onEmojiClick(event, data)} />
           </div>
       </Portal>
   })
