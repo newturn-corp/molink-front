0c81a86cd988657bffb8a012e0ca084d27e5be6b
molink-huseong <huseong.lee@molink.life>
2022-06-10 06:45:23 +0900

    emoji를 preload하지 않도록 수정

--- components/global/EmojiPickerComponent.tsx
+++ components/global/EmojiPickerComponent.tsx
@@ -24,30 +24,30 @@ export const EmojiPickerComponent: React.FC<{
               ref={pickerRef}
               className={'emoji-picker'}
               style={{
-                  userSelect: EmojiPicker.isOpen ? undefined : 'none',
-                  opacity: EmojiPicker.isOpen ? '1' : '0'
+                  userSelect: EmojiPicker.isOpen ? undefined : 'none'
               }}
               onClick={(event) => {
-                  console.log('emoji-picker click')
                   event.stopPropagation()
                   event.preventDefault()
               }}
           >
-              <EmojiPickerNoSSRWrapper
-                  groupNames={{
-                      smileys_people: '사람들',
-                      animals_nature: '자연과 동물',
-                      food_drink: '음식',
-                      travel_places: '여행과 장소',
-                      activities: '활동',
-                      objects: '물건',
-                      symbols: '심볼',
-                      flags: '국기',
-                      recently_used: '최근 사용한 이모지'
-                  }}
-                  disableSearchBar={EmojiPicker.disableSearchbar}
-                  onEmojiClick={(event, data) => EmojiPicker.onEmojiClick(event, data)}
-              />
+              {
+                  EmojiPicker.isOpen && <EmojiPickerNoSSRWrapper
+                      groupNames={{
+                          smileys_people: '사람들',
+                          animals_nature: '자연과 동물',
+                          food_drink: '음식',
+                          travel_places: '여행과 장소',
+                          activities: '활동',
+                          objects: '물건',
+                          symbols: '심볼',
+                          flags: '국기',
+                          recently_used: '최근 사용한 이모지'
+                      }}
+                      disableSearchBar={EmojiPicker.disableSearchbar}
+                      onEmojiClick={(event, data) => EmojiPicker.onEmojiClick(event, data)}
+                  />
+              }
           </div>
       </Portal>
   })
