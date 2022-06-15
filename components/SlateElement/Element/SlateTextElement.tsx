ec014844672239f252da1c9d0f242fdd5a9a6c98
molink-huseong <huseong.lee@molink.life>
2022-06-15 21:01:33 +0900

    제목 1 등 Placeholder 추가

--- components/SlateElement/Element/SlateTextElement.tsx
+++ components/SlateElement/Element/SlateTextElement.tsx
@@ -1,11 +1,12 @@
 import { observer } from 'mobx-react'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Range } from 'slate'
 import { useSelected, useSlate } from 'slate-react'
 import { TextCategory, TextElement } from '../../../Types/slate/CustomElement'
 import { isBrowser } from 'react-device-detect'
 import EditorPage from '../../../manager/Blog/Editor/EditorPage'
 import { Align } from '../../../manager/Editing/FormattingManager'
+import { isMac } from 'lib0/environment'
 
 export const SlateTextElement: React.FC<{
     attributes,
@@ -19,24 +20,46 @@ export const SlateTextElement: React.FC<{
       const selection = slateEditor.selection
       const isSelectionCollapsed = selection !== null && Range.isCollapsed(slateEditor.selection)
       const isEmpty = children[0].props.text?.text === '' && children.length === 1
-      const isHead = [TextCategory.Head1, TextCategory.Head2, TextCategory.Head3].includes(element.category)
+      const showPlaceholder = selected &&
+          isEmpty &&
+          isSelectionCollapsed &&
+          isBrowser &&
+          !editor.info.isLocked &&
+          editor.showPlaceholder &&
+          !(isMac && editor.isComposing)
 
-      return (
-          <p
-              style={{
-                  textAlign: element.align ? element.align : 'left'
-              }}
-              className={
-                  selected &&
-              isEmpty &&
-              isSelectionCollapsed &&
-              !isHead &&
-              isBrowser &&
-              !editor.info.isLocked &&
-              editor.showPlaceholder
-                      ? `${element.align === Align.Right ? 'selected-empty-text-align-right' : 'selected-empty-text'} text ${element.category}`
-                      : `text ${element.category}`} {...attributes}>
-              {children}
-          </p>
-      )
+      const getPlaceholder = useCallback(() => {
+          if (element.category === TextCategory.Head1) {
+              return '제목1'
+          } else if (element.category === TextCategory.Head2) {
+              return '제목2'
+          } else if (element.category === TextCategory.Head3) {
+              return '제목3'
+          } else {
+              return '/명령어'
+          }
+      }, [element.category])
+
+      const attrs = {
+          style: {
+              textAlign: element.align ? element.align : 'left'
+          },
+          placeholder: getPlaceholder(),
+          className: showPlaceholder
+              ? `${element.align === Align.Right ? 'selected-empty-text-align-right' : 'selected-empty-text'} text ${element.category}`
+              : `text ${element.category}`,
+          ...attributes
+      }
+
+      return (<>
+          {
+              element.category === TextCategory.Head1
+                  ? <h1 {...attrs}>{children}</h1>
+                  : element.category === TextCategory.Head2
+                      ? <h2 {...attrs}>{children}</h2>
+                      : element.category === TextCategory.Head3
+                          ? <h3 {...attrs}>{children}</h3>
+                          : <p {...attrs}>{children}</p>
+          }
+      </>)
   })
