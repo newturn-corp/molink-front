777f7eb237161f241680f1ee95674984bb645348
molink-huseong <huseong.lee@molink.life>
2022-06-16 17:16:36 +0900

    Code 입력 버그 수정

--- manager/global/Menu/MenuManager.ts
+++ manager/global/Menu/MenuManager.ts
@@ -79,7 +79,7 @@ class MenuManager {
         return true
     }
 
-    async handleEnter (event: React.KeyboardEvent, editor: Editor) {
+    handleEnter (event: React.KeyboardEvent, editor: Editor) {
         if (!this.isOpen || !this.keyboardSelectionChange) {
             return false
         }
